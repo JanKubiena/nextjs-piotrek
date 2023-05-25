b33cb65d3f22f16bcb6deac1b36d253c0fe9132e
JanKubiena <jan@kubiena.pl>
2023-05-25 20:37:38 +0200

    temporary disable photo

--- app/page.js
+++ app/page.js
@@ -7,16 +7,16 @@ export default function Home() {
   return (
     <article className="pt-0 font-sans leading-8 prose md:pt-8 md:border-t-4 prose-h1:text-2xl prose-h1:text-pw-gold prose-h1:mb-8 lg:prose-h1:mb-12 prose-a:text-pw-green prose-a:underline prose-a:decoration-1 hover:prose-a:decoration-2 prose-a:underline-offset-4 prose-a:decoration-pw-gold hover:prose-a:underline hover:prose-a:decoration-pw-gold text-md md:border-t-pw-gold">
       <h1 className="font-sans font-normal">Adwokat Piotr Tomasz Woźniak</h1>
-      <Image className='md:hidden'
+      {/* <Image className='md:hidden'
         src="/piotr-wozniak.jpg"
         priority = {true}
         width={1000}
         height={500}
         quality={80}  
         alt="Adwokat Piotr Tomasz Woźniak"
-      />
+      /> */}
       <p>
-        Szanowni Państwo,
+        Szanowni Państwo
       </p>
       <p>
         Nazywam się Piotr Tomasz Woźniak. Jestem adwokatem od 2008 r.
