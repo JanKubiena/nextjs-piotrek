ff15c8d809567083bfd99d1a5e571689518cab76
JanKubiena <jan@kubiena.pl>
2023-05-28 04:34:54 +0200

    add cookies container to layout

--- app/layout.js
+++ app/layout.js
@@ -36,35 +36,60 @@ export default function RootLayout({ children }) {
             <html lang="pl" className={`${sora.variable} ${ptserif.variable}`}>
                 <body className={`border-t-[16px] border-pw-gold overscroll-none pt-4 lg:pt-0`}>
                     <div className="max-w-[1366px] mx-auto">
-                            <Header />
+                        <Header />
 
-                            <div className="flex justify-between gap-10 px-5 pb-10 xl:gap-20 md:px-14">
-                                {/* left column */}
-                                <div className="flex-col hidden md:flex gap-y-10 shrink-0 w-55">
-                                    <Photo />
-                                    <ContactDetails />
-                                </div>
+                        <div className="flex justify-between gap-10 px-5 pb-10 xl:gap-20 md:px-14">
+                            {/* left column */}
+                            <div className="flex-col hidden md:flex gap-y-10 shrink-0 w-55">
+                                <Photo />
+                                <ContactDetails />
+                            </div>
 
-                                {/* main column */}
-                                <main className="flex flex-col w-full gap-y-12">
-                                    {children}
-                                    <div className="lg:hidden">
-                                        <SpecializationsMenu />
-                                    </div>
-                                    <div className="text-left md:hidden shrink">
-                                        <ContactDetails />
-                                    </div>
-                                </main>
-
-                                {/* right column */}
-                                <div className="hidden lg:block shrink-0 w-55">
+                            {/* main column */}
+                            <main className="flex flex-col w-full gap-y-12">
+                                {children}
+                                <div className="lg:hidden">
                                     <SpecializationsMenu />
                                 </div>
+                                <div className="text-left md:hidden shrink">
+                                    <ContactDetails />
+                                </div>
+                            </main>
+
+                            {/* right column */}
+                            <div className="hidden lg:block shrink-0 w-55">
+                                <SpecializationsMenu />
                             </div>
+                        </div>
                     </div>
+
+                    {/* <div className="fixed max-w-[1366px] mx-auto bottom-4 right-[50%] left-[50%] flex border-2 border-pw-gold bg-white">
+                        <p>
+                            Ta strona używa plików cookies (tzw. ciasteczka), które zapisują się w przeglądarce internetowej Twojego urządzenia.
+                            Dzięki nim zapewniamy prawidłowe działanie strony internetowej, a także możemy lepiej dostosować ją do preferencji użytkowników.
+                        </p>
+                    </div> */}
                     <footer className="hidden mt-8 lg:block">
                         <Footer />
                     </footer>
+                        {/* <div className="fixed bottom-4 max-w-[1366px] p-8 bg-white border border-pw-gold flex flex-col gap-5 lg:items-center lg:flex-row lg:gap-20">
+                            <p className="font-sans text-charcoal">
+                                Ta strona używa plików cookies (tzw. ciasteczka), które zapisują się w przeglądarce internetowej Twojego urządzenia.
+                                Dzięki nim zapewniamy prawidłowe działanie strony internetowej, a także możemy lepiej dostosować ją do preferencji użytkowników.
+                            </p>
+                            <button className="px-4 py-2 max-h-[42px] max-w-[110px] text-white text-sm bg-pw-green">    
+                                Rozumiem
+                            </button>
+                        </div> */}
+                        <div className="fixed bottom-4 max-w-[1254px] mx-auto border border-pw-gold bg-white inset-x-5 md:inset-x-14 p-8 flex flex-wrap lg:flex-nowrap items-center lg:justify-between gap-5 lg:gap-[160px] z-50">
+                            <p className="font-sans text-xs text-charcoal">
+                                Ta strona używa plików cookies (tzw. ciasteczka), które zapisują się w przeglądarce internetowej Twojego urządzenia.
+                                Dzięki nim zapewniamy prawidłowe działanie strony internetowej, a także możemy lepiej dostosować ją do preferencji użytkowników.
+                            </p>
+                            <button className="bg-pw-green px-4 py-2 text-white text-sm hover:bg-pw-green-800 ">
+                                Rozumiem
+                            </button>
+                        </div>
                 </body>
             </html>
         </MenuContextProvider>
