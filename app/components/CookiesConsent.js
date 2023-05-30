08e723b31a1bf73cc9b05f7364dc87937b5be624
JanKubiena <jan@kubiena.pl>
2023-05-31 01:52:24 +0200

    add working cookies

--- app/components/CookiesConsent.js
+++ app/components/CookiesConsent.js
@@ -1,25 +1,32 @@
 'use client'
 import Cookies from "js-cookie";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function CookiesConsent() {
 
+    const [consent, setConsent] = useState();
+
     useEffect(() => {
         const consent = Cookies.get("consent");
-        Cookies.set("consent", false, { expires: 365 });
+        consent === undefined && Cookies.set("consent", "false", { expires: 365 });
         // document.cookie = `consent=true; expires=Fri, 31 Dec 9999 23:59:59 GMT`
-        console.log("CookiesConsent=", consent)
+        setConsent(consent)
         },[])
 
+    const handleConsent = () => {
+        Cookies.set("consent", "true", { expires: 365 })
+
+        setConsent(Cookies.get("consent"))
+    }
 
     return (
-        <div className="fixed bottom-4 max-w-[1254px] mx-auto border border-pw-gold bg-pw-gold-50 inset-x-5 md:inset-x-14 p-8 flex flex-wrap lg:flex-nowrap items-center lg:justify-between gap-5 lg:gap-[160px] z-50">
+        <div className={consent === "false" ? "flex fixed bottom-4 max-w-[1254px] mx-auto border border-pw-gold bg-pw-gold-50 inset-x-5 md:inset-x-14 p-8 flex-wrap lg:flex-nowrap items-center lg:justify-between gap-5 lg:gap-[160px] z-50" : "hidden"}>
             <p className="font-sans text-xs text-charcoal">
                 Ta strona używa plików cookies (tzw. ciasteczka), które zapisują się w przeglądarce internetowej Twojego urządzenia.
                 Dzięki nim zapewniamy prawidłowe działanie strony internetowej, a także możemy lepiej dostosować ją do preferencji użytkowników.
             </p>
-            <button className="px-4 py-2 text-sm text-white bg-pw-green hover:bg-pw-green-800 ">
+            <button className="px-4 py-2 text-sm text-white bg-pw-green hover:bg-pw-green-800" onClick={handleConsent}>
                 Rozumiem
             </button>
         </div>
