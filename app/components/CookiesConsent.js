6229b22e033853247f0bf0b2b1c0f0a277ac34e4
JanKubiena <jan@kubiena.pl>
2023-05-31 02:15:28 +0200

    change useState default value to true in CookiesConsent

--- app/components/CookiesConsent.js
+++ app/components/CookiesConsent.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function CookiesConsent() {
 
-    const [consent, setConsent] = useState();
+    const [consent, setConsent] = useState("true");
 
     useEffect(() => {
         const consent = Cookies.get("consent");
@@ -20,15 +20,19 @@ export default function CookiesConsent() {
         setConsent(Cookies.get("consent"))
     }
 
+
+
     return (
-        <div className={consent === "true" ? "hidden" : "flex fixed bottom-4 max-w-[1254px] mx-auto border border-pw-gold bg-pw-gold-50 inset-x-5 md:inset-x-14 p-8 flex-wrap lg:flex-nowrap items-center lg:justify-between gap-5 lg:gap-[160px] z-50"}>
-            <p className="font-sans text-xs text-charcoal">
-                Ta strona używa plików cookies (tzw. ciasteczka), które zapisują się w przeglądarce internetowej Twojego urządzenia.
-                Dzięki nim zapewniamy prawidłowe działanie strony internetowej, a także możemy lepiej dostosować ją do preferencji użytkowników.
-            </p>
-            <button className="px-4 py-2 text-sm text-white bg-pw-green hover:bg-pw-green-800" onClick={handleConsent}>
-                Rozumiem
-            </button>
+        <div className={consent === "true" ? "hidden" : ""}>
+            <div className="flex fixed bottom-4 max-w-[1254px] mx-auto border border-pw-gold bg-pw-gold-50 inset-x-5 md:inset-x-14 p-8 flex-wrap lg:flex-nowrap items-center lg:justify-between gap-5 lg:gap-[160px] z-50">
+                <p className="font-sans text-xs text-charcoal">
+                    Ta strona używa plików cookies (tzw. ciasteczka), które zapisują się w przeglądarce internetowej Twojego urządzenia.
+                    Dzięki nim zapewniamy prawidłowe działanie strony internetowej, a także możemy lepiej dostosować ją do preferencji użytkowników.
+                </p>
+                <button className="px-4 py-2 text-sm text-white bg-pw-green hover:bg-pw-green-800" onClick={handleConsent}>
+                    Rozumiem
+                </button>
+            </div>
         </div>
     )
 }
