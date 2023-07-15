6a2ab0f6caf068e7c3f54a356177a06abb7b1af4
Wojtek <wojtek@szeryfy.pl>
2023-07-15 08:59:09 +0200

    add new photo

--- app/components/main/Photo.js
+++ app/components/main/Photo.js
@@ -1,17 +1,17 @@
 import Image from "next/image";
 
-
-export default function Photo(){
+export default function Photo() {
     return (
         <div className="h-55 w-full">
-            <Image className='object-cover aspect-square'
-            src="/piotr-wozniak.webp"
-            priority = {true}
-            width={220}
-            height={220}
-            quality={75}  
-            alt="Picture of the author"
+            <Image
+                className="aspect-square object-cover"
+                src="/piotrwozniak.webp"
+                priority={true}
+                width={220}
+                height={220}
+                quality={75}
+                alt="Picture of the author"
             />
         </div>
-    )
+    );
 }
--- app/page.js
+++ app/page.js
@@ -1,52 +1,55 @@
-import Image from 'next/image';
-import Link from 'next/link';
-
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <article className="pt-0 font-sans leading-8 prose md:pt-8 md:border-t-4 prose-h1:text-2xl prose-h1:text-pw-gold prose-h1:mb-8 lg:prose-h1:mb-12 prose-a:text-pw-green prose-a:underline prose-a:decoration-1 hover:prose-a:decoration-2 prose-a:underline-offset-4 prose-a:decoration-pw-gold hover:prose-a:underline hover:prose-a:decoration-pw-gold text-md md:border-t-pw-gold">
-      <h1 className="font-sans font-normal">Adwokat Piotr Tomasz Woźniak</h1>
-      <Image className='md:hidden'
-        src="/piotr-wozniak.webp"
-        priority = {true}
-        width={750}
-        height={400}
-        quality={70}  
-        alt="Adwokat Piotr Tomasz Woźniak"
-      />
-      <p>
-        Szanowni Państwo
-      </p>
-      <p>
-        Nazywam się Piotr Tomasz Woźniak. Jestem adwokatem od 2008 r.
-        prowadzącym własną Kancelarię Adwokacką. Specjalizuję się w sprawach
-        z zakresu prawa cywilnego i rodzinnego. Szczegółowy zakres spraw
-        prowadzonych przez moją Kancelarię znajdziecie Państwo w zakładce <Link href="/specjalizacje">specjalizacje</Link>.
-      </p>
-      <p>
-        Udzielam pomocy prawnej na każdym etapie sprawy zarówno w postępowaniu
-        przedprocesowym lub pozaprocesowym, jak i w trakcie postępowań sądowych.
-        O ile to możliwe, staram się sprawę rozwiązać w sposób jak najszybszy i równocześnie najmniej kosztowny dla Klienta. Zdobyte doświadczenie
-        zawodowe i życiowe pozwalają mi też w sposób rzetelny zaproponować
-        najkorzystniejszy dla Państwa sposób przeprowadzenia i zakończenia
-        sprawy.
-      </p>
-      <p>
-        Od wielu lat współpracuję z zaufanymi prawnikami o różnych
-        specjalizacjach, dzięki którym mam możliwość oceny sprawy, w przypadku
-        wyjątkowego lub nader rzadko spotykanego stanu faktycznego lub prawnego.
-      </p>
-      <p>
-        Niezależnie od zdobytego dotychczas doświadczenia cały czas poszerzam
-        swoją wiedzę uczestnicząc w licznych szkoleniach.
-      </p>
-      <p>
-        Jestem przekonany, że jestem w stanie pomóc Państwu w rozwiązaniu
-        Państwa problemów i zapraszam do współpracy.
-      </p>
-      <p>
-        Pozostaję z poważaniem
-      </p>
-    </article>
-  );
+    return (
+        <article className="text-md prose pt-0 font-sans leading-8 prose-h1:mb-8 prose-h1:text-2xl prose-h1:text-pw-gold prose-a:text-pw-green prose-a:underline prose-a:decoration-pw-gold prose-a:decoration-1 prose-a:underline-offset-4 hover:prose-a:underline hover:prose-a:decoration-pw-gold hover:prose-a:decoration-2 md:border-t-4 md:border-t-pw-gold md:pt-8 lg:prose-h1:mb-12">
+            <h1 className="font-sans font-normal">
+                Adwokat Piotr Tomasz Woźniak
+            </h1>
+            <Image
+                className="md:hidden"
+                src="/piotrwozniak.webp"
+                priority={true}
+                width={750}
+                height={400}
+                quality={70}
+                alt="Adwokat Piotr Tomasz Woźniak"
+            />
+            <p>Szanowni Państwo</p>
+            <p>
+                Nazywam się Piotr Tomasz Woźniak. Jestem adwokatem od 2008 r.
+                prowadzącym własną Kancelarię Adwokacką. Specjalizuję się w
+                sprawach z zakresu prawa cywilnego i rodzinnego. Szczegółowy
+                zakres spraw prowadzonych przez moją Kancelarię znajdziecie
+                Państwo w zakładce{" "}
+                <Link href="/specjalizacje">specjalizacje</Link>.
+            </p>
+            <p>
+                Udzielam pomocy prawnej na każdym etapie sprawy zarówno
+                w postępowaniu przedprocesowym lub pozaprocesowym, jak i w
+                trakcie postępowań sądowych. O ile to możliwe, staram się sprawę
+                rozwiązać w sposób jak najszybszy i równocześnie najmniej
+                kosztowny dla Klienta. Zdobyte doświadczenie zawodowe i życiowe
+                pozwalają mi też w sposób rzetelny zaproponować
+                najkorzystniejszy dla Państwa sposób przeprowadzenia
+                i zakończenia sprawy.
+            </p>
+            <p>
+                Od wielu lat współpracuję z zaufanymi prawnikami o różnych
+                specjalizacjach, dzięki którym mam możliwość oceny sprawy,
+                w przypadku wyjątkowego lub nader rzadko spotykanego stanu
+                faktycznego lub prawnego.
+            </p>
+            <p>
+                Niezależnie od zdobytego dotychczas doświadczenia cały czas
+                poszerzam swoją wiedzę uczestnicząc w licznych szkoleniach.
+            </p>
+            <p>
+                Jestem przekonany, że jestem w stanie pomóc Państwu w
+                rozwiązaniu Państwa problemów i zapraszam do współpracy.
+            </p>
+            <p>Pozostaję z poważaniem</p>
+        </article>
+    );
 }
