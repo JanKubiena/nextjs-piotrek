9b43cb74e98f1279cddf1b8717151fc623f12461
Wojtek <wojtek@szeryfy.pl>
2023-07-23 22:34:23 +0200

    change image to regular img

--- app/components/main/Photo.js
+++ app/components/main/Photo.js
@@ -2,16 +2,17 @@ import Image from "next/image";
 
 export default function Photo() {
   return (
-    <div className="h-55 w-full">
-      <Image
-        className="aspect-square object-cover"
+    <div className="w-full h-55">
+      {/* <Image
+        className="object-cover aspect-square"
         src="/piotrwozniak.webp"
         priority={true}
         width={220}
         height={220}
         quality={75}
         alt="Picture of the author"
-      />
+      /> */}
+      <img className="object-cover aspect-square" src="/piotrwozniak.webp" />
     </div>
   );
 }
